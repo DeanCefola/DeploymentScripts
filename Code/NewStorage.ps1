01b9de0631c0bd621695a3db9c4d185a6e93571a
Dean Cefola <Dean.Cefola@microsoft.com>
2020-12-30 18:26:23 -0500

    remove unneeded parameters
    clean up parameters

--- Code/NewStorage.ps1
+++ Code/NewStorage.ps1
@@ -12,26 +12,19 @@
 #>
 
 [Cmdletbinding()]
-Param (    
-    [Parameter(Mandatory=$true)]
-        [string]$UserName,
-    [Parameter(Mandatory=$true)]
-        [securestring]$PWD,    
+Param (         
     [Parameter(Mandatory=$true)]
         [string]$Prefix
 )
 
 begin {    
-    $Creds = New-Object system.Management.Automation.PSCredential($UserName,$PWD)    
-    Connect-AzAccount -Credential $Creds -Verbose
-    Wait-Event -Timeout 5
+    
 }
 
 Process{
     $STAccount = Get-AzStorageAccount -ResourceGroupName DeploymentScripts -Name $Prefix"storage"
     New-AzStorageShare -Name testds00 -Context $STAccount.Context 
     Set-AzStorageShareQuota -Context $STAccount.Context -Quota 10 -ShareName testds00
-
 }
 
 End {
