fa5504e2051bacafad21f8f52ba473edb6449f5e
Dean Cefola <Dean.Cefola@microsoft.com>
2021-01-18 16:53:12 -0500

    update 7 output from text to IIS

--- 7-DeployScripts/Install_IIS.ps1
+++ 7-DeployScripts/Install_IIS.ps1
@@ -12,7 +12,22 @@
 #>	
 
 
-install-windowsfeature -name Web-Server -IncludeManagementTools
-Set-Location -Path c:\inetpub\wwwroot
-Add-Content iisstart.htm "<H1><center>WELCOME to my Web Server $env:COMPUTERNAME, Azure Academy Rocks!</center></H1>"
-Invoke-command -ScriptBlock{iisreset}
+Param (
+)
+
+Begin {
+  install-windowsfeature -name Web-Server -IncludeManagementTools
+}
+
+Process {  
+  Set-Location -Path c:\inetpub\wwwroot
+  Add-Content iisstart.htm "<H1><center>WELCOME to my Web Server $env:COMPUTERNAME, Azure Academy Rocks!</center></H1>"
+  Invoke-command -ScriptBlock{iisreset}
+}
+
+End {
+  $Value = (Get-WindowsFeature -Name Web-Server).installstate
+  $Output = Write-Host "IIS is $Value"
+  $DeploymentScriptOutputs = @{}
+  $DeploymentScriptOutputs['IIS'] = $output
+}
